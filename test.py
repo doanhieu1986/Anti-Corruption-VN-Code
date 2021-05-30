61c2d3acef8e5c1d2ddc6233ac607a9319544285
doanhieu1986 <59817742+doanhieu1986@users.noreply.github.com>
2021-05-30 21:54:16 +0700

    Update test.py

--- test.py
+++ test.py
@@ -7,15 +7,22 @@ import requests
 import utils
 import nlp
 
-# test = pd.read_csv('D:/VNU/Thesis/Thesis - Python/vnexpress_all_processing.csv')
-# print(test)
+df = pd.read_csv('/Users/hieudt/VNU/Thesis/Thesis-Data_Science-main/vnexpress_all_processing.csv')
+count_chunk = 7
+for i in range(700,len(df), 100):
+    test = df.iloc[i:i+100]
+    count_chunk +=1
+    test['Dict'] = test.apply(lambda row: nlp.dict_of_words(row['Title_content']), axis=1)
+    test.to_csv('/Users/hieudt/VNU/Thesis/Thesis-Data_Science-main/vnexpress_processing_chunk_{stt}.csv'.format(stt = count_chunk),index=False, encoding='utf-8-sig')
 
-# test = pd.read_csv('/Users/hieudt/VNU/Thesis/Thesis-Data_Science-main/vnexpress_all_processing.csv',chunksize=100)
-# count_chunk = 0
+
+# count_chunk = 4
 # for i in test:
 #     count_chunk +=1
 #     i['Dict'] = i.apply(lambda row: nlp.dict_of_words(row['Title_content']), axis=1)
 #     i.to_csv('/Users/hieudt/VNU/Thesis/Thesis-Data_Science-main/vnexpress_processing_chunk_{stt}.csv'.format(stt = count_chunk),index=False, encoding='utf-8-sig')
+#     del i
+#     gc.collect()
 
 
 
@@ -23,10 +30,12 @@ import nlp
 # test['Dict'] = test.apply(lambda row: nlp.dict_of_words(row['Title_content']), axis=1)
 # test.to_csv('/Users/hieudt/VNU/Thesis/Thesis-Data_Science-main/vnexpress_all_processing_test_all.csv',index=False, encoding='utf-8-sig')
 
-x = pd.read_csv('/Users/hieudt/VNU/Thesis/Thesis-Data_Science-main/vnexpress_processing_chunk_4.csv')
-print(x)
-
-y = x['Dict'][0]
-y = ast.literal_eval(y)
-# y = nlp.uppercase_for_dict_keys(y)
-print(y)
+# x = pd.read_csv('/Users/hieudt/VNU/Thesis/Thesis-Data_Science-main/vnexpress_processing_chunk_5.csv')
+# # x['Check'] = x.apply(lambda row: nlp.check_subset(ast.literal_eval(row['Dict']),[['CHỨNG_CỨ','KHÁM_NGHIỆM'],['VKSND'],['CÔNG_AN']]), axis=1)
+# print(x)
+# g = pd.read_csv('/Users/hieudt/VNU/Thesis/Thesis-Data_Science-main/vnexpress_all_processing.csv')[400:500]
+# print(g)
+# # y = x['Dict'][0]
+# # # # y = ast.literal_eval(y)
+# # y = nlp.check_subset(ast.literal_eval(y), [['CHỨNG_CỨ','KHÁM_NGHIỆM'],['VKSND']])
+# # print(y)
