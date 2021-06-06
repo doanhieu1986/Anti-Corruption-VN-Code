870fa1aa3b1cddef781b4a563469c1b6ea694e5c
doanhieu1986 <59817742+doanhieu1986@users.noreply.github.com>
2021-06-06 22:50:14 +0700

    Delete webscraping.py

--- webscraping.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-import pandas as pd
-import utils
-
-# Get data from vnexpress.net:
-for i in range(199,2000):
-    page_links = 'https://vnexpress.net/phap-luat-p{page_number}'.format(page_number=i)
-    utils.get_data_from_page(page_links)
