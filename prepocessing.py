291b12a20cb8740758f2462463402f4c10411aa9
doanhieu1986 <59817742+doanhieu1986@users.noreply.github.com>
2021-06-06 22:48:35 +0700

    Update prepocessing.py

--- prepocessing.py
+++ prepocessing.py
@@ -22,25 +22,9 @@ import nlp
 
 
 # Check null value
-k=500
-n=600
-df = pd.read_csv('D:/VNU/Thesis/Thesis - Python/vnexpress_all_processing.csv')[k:n]
-# print(df)
 
-
-# Add period column to DataFrame
-# df['Month'] = ''
-# df['Quarter']= ''
-# df['Year']= ''
-df['Dict'] = ''
-for i in range(k,n):
-    # df['Month'][i] = utils.convert_date(df['Title_time'][i])[3]
-    # df['Quarter'][i] = utils.convert_date(df['Title_time'][i])[1]
-    # df['Year'][i] = utils.convert_date(df['Title_time'][i])[2]
-    text = df['Title_content'][i]
-    text = nlp.remove_character(text)
-    df['Dict'][i] =nlp.dict_of_words(text)
-    # print(nlp.dict_of_words(text))
-    # print(text)
-df.to_csv('D:/VNU/Thesis/Thesis - Python/vnexpress_all_processing_{x}.csv'.format(x=k),index=False, encoding='utf-8-sig')
-# print(df)
+def add_dict_column():
+    df = pd.read_csv('/Users/hieudt/VNU/Thesis/Thesis-Data_Science-main/vnexpress_all_processing.csv')
+    df = df.drop(26804, axis=0)
+    df['Dict'] = df.apply(lambda row: nlp.dict_of_words(row['Title_content']), axis=1)
+    df.to_csv('/Users/hieudt/VNU/Thesis/Thesis-Data_Science-main/data.csv',index=False, encoding='utf-8-sig')
